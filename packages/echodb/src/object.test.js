8b1cbd09ec48dcff21a7bd99073dc4de6279a802
David Boreham <david@wireline.io>
2020-05-30 07:01:36 -0600

    Basic lww test

--- packages/echodb/src/object.test.js
+++ packages/echodb/src/object.test.js
@@ -2,6 +2,8 @@
 // Copyright 2020 DxOS
 //
 
+import debug from 'debug';
+
 import { createId } from '@dxos/crypto';
 
 // import { Codec } from '@dxos/codec-protobuf';
@@ -16,6 +18,8 @@ import { mergeFeeds } from './crdt';
 // const codec = new Codec('.testing.Message')
 //   .addJson(require('./data.json'))
 //   .build();
+
+const log = debug('dxos:echo:test');
 
 // TODO(burdon): Use protobuf defs.
 test('Protobuf', () => {
@@ -129,4 +133,105 @@ test('Merge feeds', () => {
   test(mergeFeeds([feed1, feed2, feed3]));
   test(mergeFeeds([feed3, feed2, feed1]));
   test(mergeFeeds([feed2, feed3, feed1]));
+});
+
+test('Last writer wins', () => {
+  // Verify that the causally last write performed concurrently persists (montonicity property preserved).
+  // Three nodes: A, B, C.
+  // Initially A sets property value to "ValueA".
+  // That mutation is received by B and C.
+  // B changes property value to "ValueB".
+  // C changes property value to "ValueC".
+  // Mutations from B and C are now received by A.
+  // A now changes property value to "ValueAPrime".
+  // All mutations received by all nodes.
+  // Expected result: all nodes have property value == "ValueAPrime".
+
+  const createFeed = () => {
+    return [];
+  };
+
+  const createNode = (name) => {
+    log(`createNode: ${name}`);
+    return {
+      name,
+      writeFeed: createFeed(),
+      readFeeds: {}
+    };
+  };
+
+  // TODO(dboreham): Should be in ObjectModel.
+  const getMostRecentMutation = () => {
+    return 'What goes here?';
+  };
+
+  const testObjectId = createObjectId('testObjectType');
+  const testProperty = 'testProperty';
+
+  const appendMutation = (node, value) => {
+    log(`appendMutation: ${node.name}, ${value}`);
+    const message = MutationUtil.createMessage(
+      testObjectId,
+      KeyValueUtil.createMessage(testProperty, value, { depends: getMostRecentMutation() })
+    );
+    node.writeFeed.push(message);
+  };
+
+  const getCurrentValue = (node) => {
+    log('Merging:');
+    Object.keys(node.readFeeds).forEach(peer => log(`${peer}: ${JSON.stringify(node.readFeeds[peer])}`));
+    const transformedFeeds = Object.keys(node.readFeeds).map(
+      nodeName => { return { id: nodeName, messages: node.readFeeds[nodeName] }; }
+    );
+    const mergedFeeds = mergeFeeds(transformedFeeds);
+    log('Merged:');
+    log(`${JSON.stringify(mergedFeeds)}`);
+    const model = new ObjectModel().applyMutations(mergedFeeds);
+    const object = model.getObjectById(testObjectId);
+    log(`getCurrentValue: ${node.name}, ${JSON.stringify(object.properties)}`);
+    return object.properties.testProperty;
+  };
+
+  const replicateBetween = (peer1, peer2) => {
+    // Bidirectional replication
+    peer1.readFeeds[peer2.name] = peer2.writeFeed;
+    peer2.readFeeds[peer1.name] = peer1.writeFeed;
+    // Update the read copy of our own feed so processing readFeeds will find the latest messages
+    peer1.readFeeds[peer1.name] = peer1.writeFeed;
+    peer2.readFeeds[peer2.name] = peer2.writeFeed;
+  };
+
+  const nodes = {};
+  for (const nodeName of ['A', 'B', 'C']) {
+    nodes[nodeName] = createNode(nodeName);
+  }
+
+  appendMutation(nodes.A, 'ValueA');
+
+  replicateBetween(nodes.A, nodes.B);
+  replicateBetween(nodes.A, nodes.C);
+
+  appendMutation(nodes.B, 'ValueB');
+  appendMutation(nodes.C, 'ValueC');
+
+  replicateBetween(nodes.A, nodes.B);
+  replicateBetween(nodes.A, nodes.C);
+
+  appendMutation(nodes.A, 'ValueAPrime');
+
+  replicateBetween(nodes.A, nodes.B);
+  replicateBetween(nodes.A, nodes.C);
+  replicateBetween(nodes.B, nodes.C);
+
+  const terminalValues = {
+    A: getCurrentValue(nodes.A),
+    B: getCurrentValue(nodes.B),
+    C: getCurrentValue(nodes.C)
+  };
+
+  expect(terminalValues.A).toEqual(terminalValues.B);
+  expect(terminalValues.A).toEqual(terminalValues.C);
+  expect(terminalValues.B).toEqual(terminalValues.C);
+
+  expect(terminalValues.C).toEqual('ValueAPrime');
 });
