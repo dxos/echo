ba7eab6beb02c2ebb42d6023e36a808af45db221
richburdon <rich.burdon@gmail.com>
2020-05-31 10:45:29 -0400

    Configure depcheck.

--- packages/echo-db/src/proto.test.js
+++ packages/echo-db/src/proto.test.js
@@ -6,5 +6,7 @@ import { Codec } from '@dxos/codec-protobuf';
 
 test('Protos', () => {
   const codec = new Codec('dxos.echo');
+
+  // TODO(burdon): Test codec.
   expect(codec).not.toBeNull();
 });
