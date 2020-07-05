5d5e17447544e05818acff4e93fcd752fd1b3c2a
David Boreham <david@wireline.io>
2020-07-04 21:18:54 -0600

    Vector logical clock

---
+++ packages/echo-db/src/consistency.test.ts
@@ -0,0 +1,161 @@
+//
+// Copyright 2020 DxOS.org
+//
+
+import debug from 'debug';
+
+import { randomBytes } from '@dxos/crypto';
+
+import { LogicalClockStamp, Order, logOrder } from './consistency';
+
+const log = debug('dxos:echo:test');
+
+test('LogicalClockStamp:zero', () => {
+    const lcsZero = new LogicalClockStamp();
+    const lcsNonZero = new LogicalClockStamp();
+    const nonZeroVector:Map<Buffer, number> = new Map();
+    nonZeroVector.set(randomBytes(), 99999);
+    lcsNonZero._setVector(nonZeroVector);
+    const order = LogicalClockStamp.compare(lcsZero, lcsNonZero);
+    log(`compare(zero, nonZero) -> ${logOrder(order)}`);
+    expect(order).toBe(Order.AFTER);
+    const reverse = LogicalClockStamp.compare(lcsNonZero, lcsZero);
+    log(`compare(nonZero, zero) -> ${logOrder(reverse)}`);
+    expect(reverse).toBe(Order.BEFORE);
+})
+
+test('LogicalClockStamp:ordered_one_node', () => {
+    const nodeId = randomBytes();
+
+    const lcsA = new LogicalClockStamp();
+    const vectorA:Map<Buffer, number> = new Map();
+    vectorA.set(nodeId, 1);
+    lcsA._setVector(vectorA);
+
+    const lcsB = new LogicalClockStamp();
+    const vectorB:Map<Buffer, number> = new Map();
+    vectorB.set(nodeId, 2);
+    lcsB._setVector(vectorB);
+
+    const order = LogicalClockStamp.compare(lcsA, lcsB);
+    log(`compare(zero, nonZero) -> ${logOrder(order)}`);
+    expect(order).toBe(Order.AFTER);
+    const reverse = LogicalClockStamp.compare(lcsB, lcsA);
+    log(`compare(nonZero, zero) -> ${logOrder(reverse)}`);
+    expect(reverse).toBe(Order.BEFORE);
+})
+
+test('LogicalClockStamp:equal_one_nodes', () => {
+    const nodeId = randomBytes();
+
+    const lcsA = new LogicalClockStamp();
+    const vectorA:Map<Buffer, number> = new Map();
+    vectorA.set(nodeId, 1);
+    lcsA._setVector(vectorA);
+
+    const lcsB = new LogicalClockStamp();
+    const vectorB:Map<Buffer, number> = new Map();
+    vectorB.set(nodeId, 1);
+    lcsB._setVector(vectorB);
+
+    const order = LogicalClockStamp.compare(lcsA, lcsB);
+    log(`compare(zero, nonZero) -> ${logOrder(order)}`);
+    expect(order).toBe(Order.EQUAL);
+    const reverse = LogicalClockStamp.compare(lcsB, lcsA);
+    log(`compare(nonZero, zero) -> ${logOrder(reverse)}`);
+    expect(reverse).toBe(Order.EQUAL);
+})
+
+test('LogicalClockStamp:ordered_two_nodes', () => {
+    const nodeId1 = randomBytes();
+    const nodeId2 = randomBytes();
+
+    const lcsA = new LogicalClockStamp();
+    const vectorA:Map<Buffer, number> = new Map();
+    vectorA.set(nodeId1, 1);
+    vectorA.set(nodeId2, 2);
+    lcsA._setVector(vectorA);
+
+    const lcsB = new LogicalClockStamp();
+    const vectorB:Map<Buffer, number> = new Map();
+    vectorB.set(nodeId1, 2);
+    vectorB.set(nodeId2, 2);
+    lcsB._setVector(vectorB);
+
+    const order = LogicalClockStamp.compare(lcsA, lcsB);
+    log(`compare(zero, nonZero) -> ${logOrder(order)}`);
+    expect(order).toBe(Order.AFTER);
+    const reverse = LogicalClockStamp.compare(lcsB, lcsA);
+    log(`compare(nonZero, zero) -> ${logOrder(reverse)}`);
+    expect(reverse).toBe(Order.BEFORE);
+})
+
+test('LogicalClockStamp:unordered_two_nodes', () => {
+    const nodeId1 = randomBytes();
+    const nodeId2 = randomBytes();
+
+    const lcsA = new LogicalClockStamp();
+    const vectorA:Map<Buffer, number> = new Map();
+    vectorA.set(nodeId1, 1);
+    vectorA.set(nodeId2, 2);
+    lcsA._setVector(vectorA);
+
+    const lcsB = new LogicalClockStamp();
+    const vectorB:Map<Buffer, number> = new Map();
+    vectorB.set(nodeId1, 2);
+    vectorB.set(nodeId2, 1);
+    lcsB._setVector(vectorB);
+
+    const order = LogicalClockStamp.compare(lcsA, lcsB);
+    log(`compare(zero, nonZero) -> ${logOrder(order)}`);
+    expect(order).toBe(Order.CONCURRENT);
+    const reverse = LogicalClockStamp.compare(lcsB, lcsA);
+    log(`compare(nonZero, zero) -> ${logOrder(reverse)}`);
+    expect(reverse).toBe(Order.CONCURRENT);
+})
+
+test('LogicalClockStamp:unrelated_two_nodes', () => {
+    const nodeId1 = randomBytes();
+    const nodeId2 = randomBytes();
+
+    const lcsA = new LogicalClockStamp();
+    const vectorA:Map<Buffer, number> = new Map();
+    vectorA.set(nodeId1, 1);
+    lcsA._setVector(vectorA);
+
+    const lcsB = new LogicalClockStamp();
+    const vectorB:Map<Buffer, number> = new Map();
+    vectorB.set(nodeId2, 1);
+    lcsB._setVector(vectorB);
+
+    const order = LogicalClockStamp.compare(lcsA, lcsB);
+    log(`compare(zero, nonZero) -> ${logOrder(order)}`);
+    expect(order).toBe(Order.CONCURRENT);
+    const reverse = LogicalClockStamp.compare(lcsB, lcsA);
+    log(`compare(nonZero, zero) -> ${logOrder(reverse)}`);
+    expect(reverse).toBe(Order.CONCURRENT);
+})
+
+test('LogicalClockStamp:equal_two_nodes', () => {
+    const nodeId1 = randomBytes();
+    const nodeId2 = randomBytes();
+
+    const lcsA = new LogicalClockStamp();
+    const vectorA:Map<Buffer, number> = new Map();
+    vectorA.set(nodeId1, 1);
+    vectorA.set(nodeId2, 2);
+    lcsA._setVector(vectorA);
+
+    const lcsB = new LogicalClockStamp();
+    const vectorB:Map<Buffer, number> = new Map();
+    vectorB.set(nodeId1, 1);
+    vectorB.set(nodeId2, 2);
+    lcsB._setVector(vectorB);
+
+    const order = LogicalClockStamp.compare(lcsA, lcsB);
+    log(`compare(zero, nonZero) -> ${logOrder(order)}`);
+    expect(order).toBe(Order.EQUAL);
+    const reverse = LogicalClockStamp.compare(lcsB, lcsA);
+    log(`compare(nonZero, zero) -> ${logOrder(reverse)}`);
+    expect(reverse).toBe(Order.EQUAL);
+})
---
+++ packages/echo-db/src/consistency.ts
@@ -0,0 +1,148 @@
+//
+// Copyright 2020 DxOS.org
+//
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import assert from 'assert';
+import debug from 'debug';
+
+import { PartyFeeds } from './echo-feeds';
+
+const log = debug('dxos.echo.consistency');
+
+// TODO(dboreham): Separate abstract behavior from vector implementation.
+
+// TODO(dboreham): Rationalize with FeedId.
+type NodeId = Buffer;
+
+export enum Order {
+    CONCURRENT,
+    EQUAL,
+    BEFORE,
+    AFTER
+}
+
+export const logOrder = (value: Order) => {
+    switch (value) {
+        case Order.BEFORE: return 'BEFORE';
+        case Order.AFTER: return 'AFTER';
+        case Order.EQUAL: return 'EQUAL';
+        case Order.CONCURRENT: return 'CONCURRENT';
+    }
+}
+
+export class LogicalClockStamp {
+    _vector: Map<NodeId, number>
+
+    constructor () {
+      this._vector = new Map();
+    }
+
+    static zero (): LogicalClockStamp {
+      // Empty map from constructor means zero.
+      return new LogicalClockStamp();
+    }
+
+    static compare (a: LogicalClockStamp, b: LogicalClockStamp): Order {
+      const nodeIds: Set<NodeId> = new Set();
+      // TODO(dboreham): set.addAllFrom(Iteraable)? or set.union(a,b)?
+      for (const key of a._vector.keys()) {
+        nodeIds.add(key);
+      }
+      for (const key of b._vector.keys()) {
+        nodeIds.add(key);
+      }
+      let allGreaterThanOrEqual = true;
+      let allLessThanOrEqual = true;
+      let allEqual = true;
+      for (const nodeId of nodeIds) {
+        const aSeq = a._getSeqForNode(nodeId);
+        const bSeq = b._getSeqForNode(nodeId);
+        // TODO(dboreham): Remove logging when debugged.
+        log(`aSeq:${aSeq} bSeq:${bSeq}`);
+        if (aSeq != bSeq) {
+            allEqual = false;
+        }
+        if (aSeq > bSeq) {
+            allGreaterThanOrEqual = false;
+        }
+        if (aSeq < bSeq) {
+            allLessThanOrEqual = false;
+        }
+      }
+      // if order is significant
+      if (allEqual) {
+        return Order.EQUAL;
+      } else if (allGreaterThanOrEqual) {
+        return Order.AFTER;
+      } else if (allLessThanOrEqual) {
+        return Order.BEFORE;
+      } else {
+        return Order.CONCURRENT;
+      }
+    }
+
+    static totalCompare (a: LogicalClockStamp, b: LogicalClockStamp):Order {
+      const partialOrder = LogicalClockStamp.compare(a, b);
+      if (partialOrder === Order.CONCURRENT) {
+        // Break tie TODO(dboreham): Implement
+        const tieBreaker = true;
+        return tieBreaker ? Order.AFTER : Order.BEFORE;
+      } else {
+        return partialOrder;
+      }
+    }
+
+    // TODO(dboreham): Encoding scheme is a hack : use typed protocol buffer schema definition.
+    toObject(): object {
+        return Array.from(this._vector.entries()).map(([key, value]) => { return { [key.toString('hex')]: value }; });
+    }
+
+    static fromObject(source: object): LogicalClockStamp {
+        const vector = new Map();
+        const result = new LogicalClockStamp();
+        result._vector = vector;
+        // TODO(dboreham): single-function to do this? Map.fromObject()?
+        Object.entries(source).forEach( ([key, value]) => { vector.set(key,value)} );
+        return result;
+    }
+
+    log (): string {
+      // TODO(dboreham): Use DXOS lib for Buffer as Key.
+      // TODO(dboreham): Way too functional.
+      return Array.from(this._vector.entries()).map(([key, value]) => `${key.toString('hex')}:${value}`).join(', ');
+    }
+
+    // Package private methods
+    _getSeqForNode (nodeId: NodeId): number {
+      const seq = this._vector.get(nodeId);
+      return (seq === undefined) ? 0 : seq;
+    }
+
+    _setSeqForNode (nodeId: NodeId, seq: number) {
+      this._vector.set(nodeId, seq);
+    }
+
+    _setVector (vector: Map<NodeId, number>) {
+      this._vector = vector;
+    }
+}
+
+// TODO(dboreham): Add abstraction for testing.
+export class LogicalClock {
+    // Vector time stamp comes from current feed sequences
+    // Needs to update feeds as new ones are added
+    partyFeeds: PartyFeeds;
+
+    constructor (partyFeeds: PartyFeeds) {
+      this.partyFeeds = partyFeeds;
+    }
+
+    getCurrentStamp (): LogicalClockStamp {
+      const vector = new Map();
+      this.partyFeeds.getFeeds().map(feed => vector.set(feed.getKey(), feed.getSeq()));
+      const lcs = new LogicalClockStamp();
+      lcs._setVector(vector);
+      return lcs;
+    }
+}
