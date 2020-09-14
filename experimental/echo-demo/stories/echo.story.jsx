e872b0c0f2d74bcc7a3862ed7dd08ad20a3b52c7
Dmytro Maretskyi <maretskii@gmail.com>
2020-09-14 18:01:37 +0200

    Update invitations in demo

--- experimental/echo-demo/stories/echo.story.jsx
+++ experimental/echo-demo/stories/echo.story.jsx
@@ -138,16 +138,13 @@ const Test = ({ peers, showGrid = false }) => {
         log(`Inviting ${peer.id} => ${p.id} [${String(party)}]`);
 
         // Invite party.
-        const invitation = party.createInvitation();
-        log('Invitation request:', invitation.request);
+        const invitation = await party.createInvitation({
+          secretProvider: () => Buffer.from('0000'),
+          secretValidator: () => true,
+        });
+        log('Invitation request:', invitation);
 
-        // Response.
-        const responder = await p.database.joinParty(invitation.request);
-        log('Invitation response:', responder.response);
-        await invitation.finalize(responder.response);
-
-        // Invited
-        const remoteParty = responder.party;
+        const remoteParty = await p.database.joinParty(invitation, () => Buffer.from('0000'));
         await remoteParty.open();
         log('Invited Party:', String(remoteParty));
 
