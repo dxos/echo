6ba99fbc13ca3b428deccfb9c90bd75582de2a50
Dmytro Maretskyi <maretskii@gmail.com>
2020-09-29 15:37:06 +0200

    Fix test-agent

--- packages/echo-db-test/src/test-agent.ts
+++ packages/echo-db-test/src/test-agent.ts
@@ -7,8 +7,9 @@ import assert from 'assert';
 import { Keyring, KeyType } from '@dxos/credentials';
 import { keyToString } from '@dxos/crypto';
 import {
-  codec, ECHO, FeedStoreAdapter, InvitationDescriptor, Party, PartyFactory, PartyManager, IdentityManager
+  ECHO, FeedStoreAdapter, InvitationDescriptor, Party, PartyFactory, PartyManager, IdentityManager
 } from '@dxos/echo-db';
+import { codec } from '@dxos/echo-protocol';
 import { FeedStore } from '@dxos/feed-store';
 import { ModelFactory } from '@dxos/model-factory';
 import { NetworkManager } from '@dxos/network-manager';
