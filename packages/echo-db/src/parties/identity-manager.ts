5526380ad468e047e77c3c14da42322ce3e9e6c7
Thomas E Lackey <telackey@bozemanpass.com>
2020-10-22 14:51:25 -0500

    Use "log".

--- packages/echo-db/src/parties/identity-manager.ts
+++ packages/echo-db/src/parties/identity-manager.ts
@@ -2,10 +2,14 @@
 // Copyright 2020 DXOS.org
 //
 
+import debug from 'debug';
+
 import { waitForCondition } from '@dxos/async';
 import { Keyring, KeyType, Filter } from '@dxos/credentials';
 
 import { PartyInternal } from './party-internal';
+
+const log = debug('dxos:echo:identity-manager');
 
 export class IdentityManager {
   // TODO(telackey): Party here is wrong, or at least incomplete. To build KeyChains and retrieve Identity "genesis"
@@ -52,7 +56,7 @@ export class IdentityManager {
         halo.processor.feedKeys
       ) : undefined;
     } catch (e) {
-      console.warn('Unable to load device KeyChain');
+      log('Unable to locate device KeyChain.');
     }
     return keyChain;
   }
