1720fc676f85313cdde83d004e314c1eaf280609
Dmytro Maretskyi <maretskii@gmail.com>
2020-12-14 16:43:40 +0100

    Suppport custom inspect in jsonReplacer

--- packages/util/src/json.ts
+++ packages/util/src/json.ts
@@ -3,12 +3,17 @@
 //
 
 import { humanize, keyToString } from '@dxos/crypto';
+import { inspect } from 'util';
 
 /**
  * JSON.stringify replacer.
  */
 export function jsonReplacer (this: any, key: string, value: any): any {
   // TODO(burdon): Why is this represented as { type: 'Buffer', data }
+  if(value !== null && typeof value === 'object' && typeof value[inspect.custom] === 'function') {
+    return value[inspect.custom]()
+  }
+
   if (value !== null && typeof value === 'object' && value.type === 'Buffer' && Array.isArray(value.data)) {
     if (value.data.length === 32) {
       const key = Buffer.from(value.data);
