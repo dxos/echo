ec4047f84ea7a99bd0a70ec0daa267ba12ed065f
Dmytro Maretskyi <maretskii@gmail.com>
2020-12-14 16:45:15 +0100

    Lint

--- packages/util/src/json.ts
+++ packages/util/src/json.ts
@@ -2,16 +2,17 @@
 // Copyright 2020 DXOS.org
 //
 
-import { humanize, keyToString } from '@dxos/crypto';
 import { inspect } from 'util';
+
+import { humanize, keyToString } from '@dxos/crypto';
 
 /**
  * JSON.stringify replacer.
  */
 export function jsonReplacer (this: any, key: string, value: any): any {
   // TODO(burdon): Why is this represented as { type: 'Buffer', data }
-  if(value !== null && typeof value === 'object' && typeof value[inspect.custom] === 'function') {
-    return value[inspect.custom]()
+  if (value !== null && typeof value === 'object' && typeof value[inspect.custom] === 'function') {
+    return value[inspect.custom]();
   }
 
   if (value !== null && typeof value === 'object' && value.type === 'Buffer' && Array.isArray(value.data)) {
