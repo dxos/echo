48eadb2841f0eee21fedc05424bbcfe6ada4fc67
Dmytro Maretskyi <maretskii@gmail.com>
2020-12-29 14:15:08 +0200

    Rename

--- packages/echo-demo/src/useSelection.ts
+++ packages/echo-demo/src/useSelection.ts
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react'
 import { Selection } from "@dxos/echo-db";
 
 export function useSelection<T>(selector: () => Selection<T>, deps: readonly any[]): T {
-  const [firstSelection] = useState(() => selector());
-  const [data, setData] = useState(firstSelection.data);
-  useEffect(() => firstSelection.update.on(() => {
+  const [initialSelection] = useState(() => selector());
+  const [data, setData] = useState(initialSelection.data);
+  useEffect(() => initialSelection.update.on(() => {
     setData(selector().data)
   }), deps)
   return data;
