4d10a87af9c798c8176b48ad6d4dc8f41b473417
richburdon <rich.burdon@gmail.com>
2020-12-30 19:22:20 +0000

    Fix

--- packages/echo-demo/src/types.ts
+++ packages/echo-demo/src/types.ts
@@ -18,23 +18,23 @@ export const graphSelector = selection => {
 
   selection
     .select({ type: OBJECT_ORG })
-      .each(item => nodes.push({
-        id: item.id,
-        type: 'org', // TODO(burdon): OBJECT_ORG
-        title: item.model.getProperty('name')
-      }))
-      .select({ link: LINK_EMPLOYEE })
-        .each(link => links.push({
-          id: link.id,
-          source: link.source.id,
-          target: link.target.id
-        }))
-        .target()
-          .each(item => nodes.push({
-            id: item.id,
-            type: 'person', // TODO(burdon): OBJECT_PERSON
-            title: item.model.getProperty('name')
-          }));
+    .each(item => nodes.push({
+      id: item.id,
+      type: 'org', // TODO(burdon): OBJECT_ORG
+      title: item.model.getProperty('name')
+    }))
+    .select({ link: LINK_EMPLOYEE })
+    .each(link => links.push({
+      id: link.id,
+      source: link.source.id,
+      target: link.target.id
+    }))
+    .target()
+    .each(item => nodes.push({
+      id: item.id,
+      type: 'person', // TODO(burdon): OBJECT_PERSON
+      title: item.model.getProperty('name')
+    }));
 
   return { nodes, links };
 };
