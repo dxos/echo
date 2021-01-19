578e5ebe212f200bff65aa7c9b47e450b8e412f2
Rich Burdon <rich.burdon@gmail.com>
2021-01-18 19:04:45 -0500

    Restructure.

--- packages/echo-demo/src/components/index.ts
+++ packages/echo-demo/src/components/index.ts
@@ -2,10 +2,9 @@
 // Copyright 2020 DXOS.org
 //
 
-export { default as CardView, ItemCard } from './CardView';
+export * from './views';
+
 export { default as DebugItemList } from './DebugItemList';
 export { default as EchoGraph } from './EchoGraph';
-export { default as GraphView } from './GraphView';
-export { default as ListView } from './ListView';
 export { default as MemberList } from './MemberList';
 export { default as SearchBar } from './SearchBar';
---
+++ packages/echo-demo/src/components/views/index.ts
@@ -0,0 +1,7 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+export { default as CardView, ItemCard } from './CardView';
+export { default as GraphView } from './GraphView';
+export { default as ListView } from './ListView';
