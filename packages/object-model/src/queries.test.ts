b32d32f8ff3268e288365328c0aa9f64d7de413e
Rich Burdon <rich.burdon@gmail.com>
2021-01-20 11:49:35 -0500

    Queries.

--- packages/object-model/src/queries.test.ts
+++ packages/object-model/src/queries.test.ts
@@ -27,6 +27,20 @@ test('Basic queries', () => {
     },
     {
       root: {
+        op: Predicate.Operation.NOT,
+        predicates: [
+          {
+            op: Predicate.Operation.EQUALS,
+            key: 'name',
+            value: {
+              string: 'item-1'
+            }
+          }
+        ]
+      }
+    },
+    {
+      root: {
         op: Predicate.Operation.OR,
         predicates: [
           {
@@ -99,16 +113,45 @@ test('Basic queries', () => {
           }
         ]
       }
+    },
+    {
+      root: {
+        op: Predicate.Operation.PREFIX_MATCH,
+        key: 'name',
+        value: {
+          string: 'item'
+        }
+      }
+    },
+    {
+      root: {
+        op: Predicate.Operation.TEXT_MATCH,
+        key: 'description',
+        value: {
+          string: 'dx'
+        }
+      }
+    },
+    {
+      root: {
+        op: Predicate.Operation.TEXT_MATCH,
+        key: 'description',
+        value: {
+          string: ''
+        }
+      }
     }
   ];
 
   const items = [
     {
-      name: 'item-0'
+      name: 'item-0',
+      description: 'this should not match any text queries.'
     },
     {
       name: 'item-1',
-      label: 'red'
+      label: 'red',
+      description: 'this item -- references  dxos  projects.'
     },
     {
       name: 'item-2',
@@ -127,7 +170,11 @@ test('Basic queries', () => {
     [],
     [items[0]],
     [items[0], items[2]],
+    [items[0], items[2]],
     [items[1], items[2]],
-    [items[0], items[1], items[2]]
+    [items[0], items[1], items[2]],
+    [items[0], items[1], items[2]],
+    [items[1]],
+    []
   ]);
 });
--- packages/object-model/src/queries.ts
+++ packages/object-model/src/queries.ts
@@ -16,24 +16,62 @@ export class QueryProcessor {
     private readonly _getter: Getter
   ) {}
 
+  /**
+   * Returns true if item matches the query.
+   */
   match (item: any): boolean {
     return this._match(item, this._query.root!);
   }
 
   _match (item: any, predicate: Predicate): boolean {
     switch (predicate.op) {
+      //
+      // Boolean operators
+      //
+
       case Predicate.Operation.OR: {
-        return !(predicate.predicates!.findIndex((predicate: Predicate) => this._match(item, predicate)) === -1);
+        return predicate.predicates!.findIndex((predicate: Predicate) => this._match(item, predicate)) !== -1;
+      }
+
+      case Predicate.Operation.AND: {
+        return predicate.predicates!.findIndex((predicate: Predicate) => !this._match(item, predicate)) === -1;
+      }
+
+      case Predicate.Operation.NOT: {
+        return predicate.predicates!.findIndex((predicate: Predicate) => !this._match(item, predicate)) !== -1;
+      }
+
+      //
+      // Equivalence
+      //
+
+      case Predicate.Operation.IN: {
+        const values = ValueUtil.valueOf(predicate.value!) || [];
+        const value = this._getter(item, predicate.key!);
+        return value && values.indexOf(value) !== -1;
       }
 
       case Predicate.Operation.EQUALS: {
         return this._getter(item, predicate.key!) === ValueUtil.valueOf(predicate.value!);
       }
 
-      case Predicate.Operation.IN: {
-        const values = ValueUtil.valueOf(predicate.value!) || [];
+      case Predicate.Operation.PREFIX_MATCH: {
         const value = this._getter(item, predicate.key!);
-        return value && values.indexOf(value) !== -1;
+        if (typeof value === 'string') {
+          const match = ValueUtil.valueOf(predicate.value!)?.toLowerCase();
+          return match && value.toLowerCase().indexOf(match) === 0;
+        }
+        break;
+      }
+
+      case Predicate.Operation.TEXT_MATCH: {
+        const value = this._getter(item, predicate.key!);
+        if (typeof value === 'string') {
+          const words = value.toLowerCase().split(/\s+/);
+          const match = ValueUtil.valueOf(predicate.value!)?.toLowerCase();
+          return match && words.findIndex(word => word.indexOf(match) === 0) !== -1;
+        }
+        break;
       }
     }
 
